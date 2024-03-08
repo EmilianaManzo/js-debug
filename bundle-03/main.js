2db504d193c30fba6de983a3af6c3fe5be84d007
EmilianaManzo <emiliana.ma96@gmail.com>
2024-03-08 16:30:23 +0100

    [add] bundle 3

--- bundle-03/main.js
+++ bundle-03/main.js
@@ -84,3 +84,91 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+
+
+
+// corretto
+// const cars = [
+//     {
+//         manufacturer: 'Ford',
+//         model: 'Fiesta',
+//         type: 'diesel'
+//     },
+//     {
+//         manufacturer: 'Audi',
+//         model: 'A1',
+//         type: 'benzina'
+//     },
+//     {
+//         manufacturer: 'Volkswagen',
+//         model: 'Golf',
+//         type: 'Benzina'
+//     },
+//     {
+//         manufacturer: 'Fiat',
+//         model: 'Panda',
+//         type: 'metano'
+//     },
+//     {
+//         manufacturer: 'Fiat',
+//         model: 'Multipla',
+//         type: 'GPL'
+//     },
+//     {
+//         manufacturer: 'Tesla',
+//         model: 'Model 3',
+//         type: 'elettrico'
+//     },
+//     {
+//         manufacturer: 'Volkswagen',
+//         model: 'Polo',
+//         type: 'benzina'
+//     },
+//     {
+//         manufacturer: 'Ford',
+//         model: 'Kuga',
+//         type: 'Diesel'
+//     },
+//     {
+//         manufacturer: 'Seat',
+//         model: 'Ibiza',
+//         type: 'metano'
+//     },
+//     {
+//         manufacturer: 'Audi',
+//         model: 'R8',
+//         type: 'Benzina'
+//     }
+// ];
+
+// const gasolineCars = cars.filter( (auto) => auto.type = 'benzina');
+
+// const dieselCars = cars.filter( (auto) => {
+//     auto.type = 'diesel';
+// });
+
+// const otherCars = cars.filter( (auto) => {
+//     return auto.type !== 'benzina' && auto.type !== 'diesel';
+// });
+
+// console.log('Auto a benzina');
+// console.log('*******************************');
+// console.log(gasolineCars);
+
+// console.log('Auto a diesel');
+// console.log('*******************************');
+// console.log(dieselCars);
+
+// console.log('Tutte le altre auto');
+console.log(otherCars)
+
+/**
+1. da un array di oggetti, filtrale auto che sono a benzina, a diesel e tutte le altre auto e le divide in nuovi array  
+utilizzando filter
+2. si :
+    -le due virgole nell'array 
+    -la sintassi della freccia dell'arrow function 
+3. si :
+    -deve esserci un solo uguale nelle parentesi di riga 67 e 70 perche sono assegnazioni 
+    -al posto dell'operatore logico or || deve esserci l'operatore logico and && 
+ */
